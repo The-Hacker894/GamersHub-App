8456b5d77f6622dc1ebb1371d1a67bbaeca0a313
The-Hacker894 <skylarmccauley864@gmail.com>
2018-07-05 16:15:32 -0400

    v1.3.X

--- app/src/main/java/com/hackerlabs/sky/gamershub/EnglishHome.java
+++ app/src/main/java/com/hackerlabs/sky/gamershub/EnglishHome.java
@@ -1,6 +1,8 @@
 package com.hackerlabs.sky.gamershub;
 
+
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
@@ -10,33 +12,79 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+
 public class EnglishHome extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ImageView Discord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_english);
-    }
-    public void goToSocialLinks (View v) {
-        boolean connected = false;
-        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
-                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
-            startActivity(new Intent(EnglishHome.this, Social.class));
 
-            connected = true;
-        }
-        else
+        Discord = (ImageView) findViewById(R.id.DiscordButton);
 
-            Toast.makeText(this, "You need an Internet Connection for this!",
-                    Toast.LENGTH_LONG).show();
+        Discord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customDialog(R.string.discord_server, R.string.discord_server_question, "GamersHub Renaissance", "GamersHub");
 
-        connected = false;
-
+            }
+        });
 
     }
+
+    /**
+     * Alert Dialog
+     * @param title
+     * @param message
+     * @param cancelMethod
+     * @param okMethod
+
+     */
+
+    public void customDialog(int title, int message, final String cancelMethod, final String okMethod) {
+        final android.support.v7.app.AlertDialog.Builder builderSingle = new android.support.v7.app.AlertDialog.Builder(this);
+        builderSingle.setIcon(R.drawable.ic_dns_black_24dp);
+        builderSingle.setTitle(title);
+        builderSingle.setMessage(message);
+
+        builderSingle.setNegativeButton(
+                "GamersHub",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(cancelMethod.equals("cancelMethod1")) {
+                            goToUrl ( "https://discord.gg/RkZ62QH");
+                        } else {
+                            goToUrl("https://discord.gg/Ak52Axg");
+
+                        }
+                    }
+                });
+        builderSingle.setPositiveButton(
+                "GamersHub Renaissance",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(okMethod.equals("okMethod1")) {
+                            goToUrl("https://discord.gg/Ak52Axg");
+                        } else {
+                            goToUrl ( "https://discord.gg/RkZ62QH");
+
+                        }
+                    }
+
+                });
+        builderSingle.show();
+    }
+
     public void goToWebsite (View v) {
         boolean connected = false;
         ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -48,7 +96,7 @@ public class EnglishHome extends AppCompatActivity {
         }
         else
 
-            Toast.makeText(this, "You need an Internet Connection for this!",
+            Toast.makeText(this, R.string.internet_error,
                     Toast.LENGTH_LONG).show();
 
         connected = false;
@@ -57,12 +105,6 @@ public class EnglishHome extends AppCompatActivity {
         startActivity(new Intent(EnglishHome.this, Home.class));
     }
 
-    public void goToDiscord (View view) {
-        goToUrl ( "https://discord.io/frgamershub");
-    }
-    public void goToGuilded (View view) {
-        goToUrl( "https://twitter.com/GamersHub6");
-    }
     public void goToTwitter (View view) {
         goToUrl( "https://twitter.com/GamersHub1");
     }
--- app/src/main/java/com/hackerlabs/sky/gamershub/Home.java
+++ app/src/main/java/com/hackerlabs/sky/gamershub/Home.java
@@ -1,6 +1,7 @@
 package com.hackerlabs.sky.gamershub;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
@@ -10,33 +11,76 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+            private ImageView Discord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Discord = (ImageView) findViewById(R.id.DiscordButton);
+
+        Discord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customDialog(R.string.discord_server_fr, R.string.discord_server_question_fr, "GamersHub Renaissance", "GamersHub");
+
+            }
+        });
     }
-    public void goToSocialLinks (View v) {
-        boolean connected = false;
-        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
-                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
-            startActivity(new Intent(Home.this, Social.class));
 
-            connected = true;
-        }
-        else
+    /**
+     * Alert Dialog
+     * @param title
+     * @param message
+     * @param cancelMethod
+     * @param okMethod
 
-            Toast.makeText(this, "You need an Internet Connection for this!",
-                    Toast.LENGTH_LONG).show();
+     */
+    public void customDialog(int title, int message, final String cancelMethod, final String okMethod) {
+        final android.support.v7.app.AlertDialog.Builder builderSingle = new android.support.v7.app.AlertDialog.Builder(this);
+        builderSingle.setIcon(R.drawable.ic_dns_black_24dp);
+        builderSingle.setTitle(title);
+        builderSingle.setMessage(message);
 
-        connected = false;
+        builderSingle.setNegativeButton(
+                "GamersHub",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(cancelMethod.equals("cancelMethod1")) {
+                            goToUrl ( "https://discord.gg/RkZ62QH");
+                        } else {
+                            goToUrl("https://discord.gg/Ak52Axg");
 
+                        }
+                    }
+                });
+        builderSingle.setPositiveButton(
+                "GamersHub Renaissance",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(okMethod.equals("okMethod1")) {
+                            goToUrl("https://discord.gg/Ak52Axg");
+                        } else {
+                            goToUrl ( "https://discord.gg/RkZ62QH");
 
+                        }
+                    }
+
+                    });
+        builderSingle.show();
     }
+
     public void goToWebsite (View v) {
         boolean connected = false;
         ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -48,7 +92,7 @@ public class Home extends AppCompatActivity {
         }
         else
 
-            Toast.makeText(this, "You need an Internet Connection for this!",
+            Toast.makeText(this, R.string.internet_error_fr,
                     Toast.LENGTH_LONG).show();
 
         connected = false;
@@ -57,9 +101,6 @@ public class Home extends AppCompatActivity {
         startActivity(new Intent(Home.this, EnglishHome.class));
     }
 
-    public void goToDiscord (View view) {
-        goToUrl ( "https://discord.io/frgamershub");
-    }
     public void goToTwitter (View view) {
         goToUrl( "https://twitter.com/GamersHub1");
     }
